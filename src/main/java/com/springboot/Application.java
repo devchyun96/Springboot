7c1c9126b14a8d800be26bbbdaf14aa90c0e6cc7
ChoiKangHyun <chlhun416@gmail.com>
2023-04-04 14:50:15 +0900

    travis

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -2,11 +2,14 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+       SpringApplication application=new SpringApplication(Application.class);
+       application.addListeners(new ApplicationPidFileWriter());
+       application.run(args);
     }
 }
